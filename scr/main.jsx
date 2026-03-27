8761957378c1378efde7f6fc28c35c8b3fc90d65
Tsadok <adidomahukpeou@gmail.com>
2026-03-27 03:40:31 +0100

    Delete main.jsx

--- scr/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BirthdayApp from './BirthdayLink'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BirthdayApp />
-  </React.StrictMode>
-)
