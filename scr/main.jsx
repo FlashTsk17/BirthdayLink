d104f52949d966e1b171dc0a138070a4a5daa036
Tsadok <adidomahukpeou@gmail.com>
2026-03-27 01:16:16 +0100

    Add main.jsx to render BirthdayApp component

---
+++ scr/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BirthdayApp from './BirthdayLink'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BirthdayApp />
+  </React.StrictMode>
+)
